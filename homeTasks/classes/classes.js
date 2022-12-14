47d1f0e8204ec0f104af6acd624bd65b76291abd
vetalredrok <vetalredrok@gmail.com>
2022-12-15 00:47:55 +0200

    classes

--- homeTasks/classes/classes.js
+++ homeTasks/classes/classes.js
@@ -1,3 +1,5 @@
+//Create 2 objects: animal and cat, add move property to animal object, cat object must inherit move property from object animal
+
 class Animal {
     constructor(name) {
         this.name = name;
